71c465b83323932890ff318749339c1dee7e513e
Tobias Kunze <r@rixx.de>
2021-01-16 11:36:29 +0100

    Run black

--- pretix_limit_phone_country/__init__.py
+++ pretix_limit_phone_country/__init__.py
@@ -9,20 +9,22 @@ __version__ = '1.0.0'
 
 
 class PluginApp(PluginConfig):
-    name = 'pretix_limit_phone_country'
-    verbose_name = 'Limit Phone Number Country'
+    name = "pretix_limit_phone_country"
+    verbose_name = "Limit Phone Number Country"
 
     class PretixPluginMeta:
-        name = gettext_lazy('Limit Phone Number Country')
-        author = 'Tobias Kunze'
-        description = gettext_lazy('Limit the available phone number country code in pretix phone number questions. Caution! Applies to the whole server!')
+        name = gettext_lazy("Limit Phone Number Country")
+        author = "Tobias Kunze"
+        description = gettext_lazy(
+            "Limit the available phone number country code in pretix phone number questions. Caution! Applies to the whole server!"
+        )
         visible = True
         version = __version__
-        category = 'FEATURE'
+        category = "FEATURE"
         compatibility = "pretix>=2.7.0"
 
     def ready(self):
         from . import signals  # NOQA
 
 
-default_app_config = 'pretix_limit_phone_country.PluginApp'
+default_app_config = "pretix_limit_phone_country.PluginApp"
--- setup.py
+++ setup.py
@@ -8,35 +8,34 @@ from pretix_limit_phone_country import __version__
 
 
 try:
-    with open(os.path.join(os.path.dirname(__file__), 'README.rst'), encoding='utf-8') as f:
+    with open(
+        os.path.join(os.path.dirname(__file__), "README.rst"), encoding="utf-8"
+    ) as f:
         long_description = f.read()
 except:
-    long_description = ''
+    long_description = ""
 
 
 class CustomBuild(build):
     def run(self):
-        management.call_command('compilemessages', verbosity=1)
+        management.call_command("compilemessages", verbosity=1)
         build.run(self)
 
 
-cmdclass = {
-    'build': CustomBuild
-}
+cmdclass = {"build": CustomBuild}
 
 
 setup(
-    name='pretix-limit-phone-country',
+    name="pretix-limit-phone-country",
     version=__version__,
-    description='Limit the available phone number country code in pretix phone number questions. Caution! Applies to the whole server!',
+    description="Limit the available phone number country code in pretix phone number questions. Caution! Applies to the whole server!",
     long_description=long_description,
-    url='https://github.com/rixx/pretix-limit-phone-country',
-    author='Tobias Kunze',
-    author_email='r@rixx.de',
-    license='Apache',
-
+    url="https://github.com/rixx/pretix-limit-phone-country",
+    author="Tobias Kunze",
+    author_email="r@rixx.de",
+    license="Apache",
     install_requires=[],
-    packages=find_packages(exclude=['tests', 'tests.*']),
+    packages=find_packages(exclude=["tests", "tests.*"]),
     include_package_data=True,
     cmdclass=cmdclass,
     entry_points="""
